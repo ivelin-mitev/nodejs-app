6736e6708c64e9efb8328aef1c1d66cf41fef3f5
Ivelin Mitev <ivelin.mitev@mentormate.com>
2022-11-20 22:15:36 +0200

    Refactor functionallity

--- index.js
+++ index.js
@@ -1,34 +1,71 @@
-const express = require('express');
+const express = require("express");
+const socket = require("socket.io");
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
 
-const { Server } = require("socket.io");
-const io = new Server(server);
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
+app.get('/', function (req, res, next) {
+  // Handle the get for this route
+});
 
-//When user connects
-io.on('connection', (socket) => {
-    // We show in the console that user connected
-    console.log('a user connected');
+app.post('/', function (req, res, next) {
+  // Handle the post for this route
+});
 
-    // When the connected user disconnects
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+const server = app.listen(3000, function () {
+  console.log("server running on port 3000");
+});
+
+const io = socket(server, {
+  allowEIO3: true,
+  cors: { credentials: true, origin: 'http://localhost:3000' },
+});
+
+let game = {};
+
+io.on("connection", function (socket) {
+
+  // If a player joins from URL invitation
+  socket.on("invitedPlayerLoading", function (roomId) {
+    const ROOMID = roomId;
+    socket.emit('playerHasInvitation', true);
+    socket.emit('newPlayerId', game[ROOMID].players.length);
+  })
+
+  // If a player doesn't have invitation URL
+  socket.on("initialPlayer", function () {
+    socket.emit('playerHasInvitation', false);
+    // On connection a room ID is created
+    const currentRoomId = socket.id.substr(0, 6);
+    // We emit the room and add it to the route as query
+    // so the url can be copied and shared for other players
+    io.emit('roomId', currentRoomId);
+    // io.emit('set_player_id', game.players.length);
+
+    // On clicking the play button, a waiting room is initialized
+    // and the ID becomes the "key" in the game object, while the value
+    // will be all the information about the current instance of the game as
+    // players, roles of players, question, winner.
+    socket.on("initialize_waiting_room", function (data) {
+      game[currentRoomId] = {
+        players: [],
+        phase: 'waiting',
+        winner: undefined,
+        strawsDrown: 0
+      }
+      game[currentRoomId].players.push(data);
+      console.log(game[currentRoomId].players.length);
     })
 
-    //When we receive 'chat message' event
-    socket.on('chat message', (msg, userName) => {
-        io.emit('chat message', msg, userName);
-    })
+  })
 
-
-  });
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+  // Basic tests
+  console.log('connected ' + socket.id);
+  socket.on("disconnect", function () {
+    console.log('disconnect');
+  })
 });
